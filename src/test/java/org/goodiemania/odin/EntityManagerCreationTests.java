c109a64654bc7eaca58224f5567312c568cfd5d7
Thomas Goodwin <thomas@goodiemania.org>
2020-09-29 23:31:55 +1300

    rename test set that only test entity manager creation

--- src/test/java/org/goodiemania/odin/EntityManagerCreationTests.java
+++ src/test/java/org/goodiemania/odin/EntityManagerCreationTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class EntityManagerTests {
+public class EntityManagerCreationTests {
     private Odin odin;
 
     @BeforeEach
